982619369ea995542d2450cb54375b3c24171fe2
laura fox <laurafox91@gmail.com>
2024-04-10 21:32:17 +0000

    bones of the game in place

--- run.py
+++ run.py
@@ -8,24 +8,24 @@ def welcome_msg():
     Welcome message to greet the player and explain the rules.
     """
     print("")
-    print("┌ ─  - ┐")
-    print("| ●    |")
-    print("|    ● |")
-    print("└ - -  ┘")
-    print("*** Welcome To Dice Roll ***")
-    print("┌ ─  - ┐")
-    print("| ●    |")
-    print("|    ● |")
-    print("└ - -  ┘\n")
+    print("┌ ─  - ┐  ┌ ─  - ┐")
+    print("| ●    |  | ●    |")
+    print("|    ● |  |    ● |")
+    print("└ - -  ┘  └ - -  ┘")
+    print("*** Welcome To Double Dice ***")
+    print("┌ ─  - ┐  ┌ ─  - ┐")
+    print("| ●    |  | ●    |")
+    print("|    ● |  |    ● |")
+    print("└ - -  ┘  └ - -  ┘\n")
     
     player = get_player_name()
 
     print(f"\nAlright, {player} the rules for this game are:")
     print(
         '''
-        The aim is to roll the highest value...
-        You will take turns against the computer...
-        The highest score wins!
+        The aim is to roll 2 of the same numbers...
+        Double or nothing...
+        Good luck!
         ''')
 
     return player
@@ -54,14 +54,41 @@ def start_game(player):
 
         if start_answer == "y":
             print("\nLets roll...")
+            time.sleep(1)
             break
         elif start_answer == "n":
             print("\nSee you on the next roll")
-            break
+            quit()
         else:
             print("\nPlease answer y or n\n")
 
+def play_game(player):
+    roll_again = "y"
+    while roll_again == "y":
+        print("Rolling the dice...")
+        time.sleep(1)
 
+        dice1 = random.randint(1,6)
+        dice2 = random.randint(1,6)
+        print("The values are: ")
+        print("Dice 1 =", dice1)
+        print("Dice 2 =", dice2)
 
-player = welcome_msg()
-start_game(player)
+        if dice1 == dice2:
+            print("You rolled a double! You win!")
+        else:
+            print("Keep trying")
+    
+        roll_again = input("Roll the dice again? y/n \n")
+        if roll_again == "n":
+            print("See you on the next roll!")
+            print("Your total score this game: ")
+            quit()        
+
+def main():
+    player = welcome_msg()
+    start_game(player)
+    play_game(player)
+
+ 
+main()  
