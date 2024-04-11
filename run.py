174b17a2a5d239747cd4690bd12bdf705250713a
laura fox <laurafox91@gmail.com>
2024-04-11 15:33:01 +0000

    python formatter

--- run.py
+++ run.py
@@ -1,6 +1,12 @@
 import random
 import time
 import os
+
+# pypi.org/project/colorama/ Instructions on adding color to the terminal
+import colorama
+
+colorama.init(autoreset=True)
+
 # ● ┌ ─ ┐ │ └ ┘ Dice pieces for welcome message
 
 
@@ -9,25 +15,26 @@ def welcome_msg():
     Welcome message to greet the player and explain the rules.
     """
     print("")
-    print("┌ ─  - ┐  ┌ ─  - ┐")
-    print("| ●    |  | ●    |")
-    print("|    ● |  |    ● |")
-    print("└ - -  ┘  └ - -  ┘")
+    print("\033[31m" + "┌ ─  - ┐  ┌ ─  - ┐")
+    print("\033[31m" + "| ●    |  | ●    |")
+    print("\033[31m" + "|    ● |  |    ● |")
+    print("\033[31m" + "└ - -  ┘  └ - -  ┘")
     print("*** Welcome To Double Dice ***")
-    print("┌ ─  - ┐  ┌ ─  - ┐")
-    print("| ●    |  | ●    |")
-    print("|    ● |  |    ● |")
-    print("└ - -  ┘  └ - -  ┘\n")
-    
+    print("\033[31m" + "┌ ─  - ┐  ┌ ─  - ┐")
+    print("\033[31m" + "| ●    |  | ●    |")
+    print("\033[31m" + "|    ● |  |    ● |")
+    print("\033[31m" + "└ - -  ┘  └ - -  ┘\n")
+
     player = get_player_name()
 
     print(f"\nAlright, {player} the rules for this game are:")
     print(
-        '''
+        """
         The aim is to roll 2 of the same numbers...
         Double or nothing...
         Try to beat your highest score!
-        ''')
+        """
+    )
 
     return player
 
@@ -46,13 +53,16 @@ def get_player_name():
         else:
             print("You can only use letters, please try again")
 
+
 def view_scoreboard(player):
     """
     A function to view previous high scores
     """
     while True:
 
-        view_scoreboard = input("Would you like to view previous highest score? y or n: ")
+        view_scoreboard = input(
+            "Would you like to view previous highest score? y or n: "
+        )
 
         if view_scoreboard == "y":
             print("Your highest score is:", load_high_score())
@@ -64,6 +74,7 @@ def view_scoreboard(player):
         else:
             print("\nPlease answer y or n: \n")
 
+
 def start_game(player):
     """
     A function to check if the user is ready to start the game
@@ -73,14 +84,15 @@ def start_game(player):
         start_answer = input("Start new game select y or n: ")
 
         if start_answer == "y":
-            print("\nLets roll...")
+            print("\033[31m" + "\nLets roll...")
             time.sleep(1)
             break
         elif start_answer == "n":
-            print("\nSee you on the next roll")
+            print("\033[31m" + "\nSee you on the next roll")
             quit()
         else:
             print("\nPlease answer y or n: \n")
+
 
 def play_game(player):
     roll_again = "y"
@@ -89,54 +101,53 @@ def play_game(player):
         print("Rolling the dice...")
         time.sleep(1)
 
-        dice1 = random.randint(1,6)
-        dice2 = random.randint(1,6)
+        dice1 = random.randint(1, 6)
+        dice2 = random.randint(1, 6)
         print("The values are: ")
         print("Dice 1 =", dice1)
         print("Dice 2 =", dice2)
 
         if dice1 == dice2:
-            print("You rolled a double! You win!")
-            score += 1 
+            print("\033[32m" + "You rolled a double! You win!")
+            score += 1
         else:
-            print("Keep trying")
-    
-    
-    
+            print("\033[31m" + "Keep trying")
+
         roll_again = input("Roll the dice again? y/n \n")
         if roll_again == "n":
-            print("See you on the next roll!")
+            print("\033[31m" + "See you on the next roll!")
             print(f"Your total score this game: {score}")
             save_high_score(score)
-            quit()        
+            quit()
+
 
 # Adaption from Quora questions and answers on scoreboard trackers
-def save_high_score(score): 
+def save_high_score(score):
     """
     Saves the highest scores to an external text file
     """
-    with open("high_score.txt", "w") as file: 
-        file.write(str(score)) 
- 
- 
-def load_high_score(): 
+    with open("high_score.txt", "w") as file:
+        file.write(str(score))
+
+
+def load_high_score():
     """
     Gives an option to view previous high scores when game starts
     """
-    if os.path.exists("high_score.txt"): 
-        with open("high_score.txt", "r") as file: 
-            return int(file.read()) 
-    else: 
+    if os.path.exists("high_score.txt"):
+        with open("high_score.txt", "r") as file:
+            return int(file.read())
+    else:
         return 0
 
+
 def main():
-    
+
     player = welcome_msg()
     view_scoreboard(player)
     start_game(player)
     play_game(player)
     load_high_score()
-    
 
- 
-main()  
+
+main()
